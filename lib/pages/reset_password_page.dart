387e6131dae6a521bcdb0e730b0f229d7766f9d7
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-10-31 17:52:48 +0200

    Clean up existing code

---
+++ lib/pages/reset_password_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class ResetPasswordPage extends StatelessWidget {
+  const ResetPasswordPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: const Text(
+          'Reset password',
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Icon(
+                Icons.lock_outline_rounded,
+                color: Colors.grey,
+                size: 144.0,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                'Reset your Password',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Enter your phone number below to reset your password',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.90,
+                child: const TextField(
+                  cursorColor: Colors.teal,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.teal,
+                      ),
+                    ),
+                    label: Text(
+                      'Phone number',
+                    ),
+                    floatingLabelStyle: TextStyle(
+                      color: Colors.teal,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  child: const Text(
+                    'SUBMIT',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.teal,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
