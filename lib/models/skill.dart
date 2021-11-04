e3baa640d25d01c1b5c1ed57c210e6a6786a28a5
night-crow <afhan2000@gmail.com>
2021-11-04 17:44:33 +0200

    added skill and job class
    please check the skill and user reletionship.

--- lib/models/skill.dart
+++ lib/models/skill.dart
@@ -1,4 +1,39 @@
 class Skill{
 
-  Skill();
+  int _skillID;
+  //Worker worker;
+  Job _job;
+
+  Skill(this._skillID, this._job);
+
+  Job get job => _job;
+
+  set job(Job value) {
+    _job = value;
+  }
+
+  int get skillID => _skillID;
+
+  set skillID(int value) {
+    _skillID = value;
+  }
+}
+
+class Job{
+  int _jobID;
+  String _Title;
+
+  Job(this._jobID, this._Title);
+
+  String get Title => _Title;
+
+  set Title(String value) {
+    _Title = value;
+  }
+
+  int get jobID => _jobID;
+
+  set jobID(int value) {
+    _jobID = value;
+  }
 }
