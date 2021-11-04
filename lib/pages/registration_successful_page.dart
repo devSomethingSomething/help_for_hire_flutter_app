2129fb37f68450e99fc669d37e75bab208ae7ca2
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-11-04 13:42:27 +0200

    Update registration successful page

--- lib/pages/registration_successful_page.dart
+++ lib/pages/registration_successful_page.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:help_for_hire_flutter_app/widgets/app_bar_widget.dart';
 
 class RegistrationSuccess extends StatefulWidget {
-  const RegistrationSuccess({Key? key}) : super(key: key);
+  const RegistrationSuccess();
 
   @override
   _RegistrationSuccessState createState() => _RegistrationSuccessState();
@@ -13,46 +11,50 @@ class RegistrationSuccess extends StatefulWidget {
 class _RegistrationSuccessState extends State<RegistrationSuccess> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBarWidget(
-          title: 'Registration Successful',
-        ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image(
-                  height: 150,
-                  width: 150,
-                  image: AssetImage('assets/images/Green_Check.png'),
+    return Scaffold(
+      appBar: const AppBarWidget(
+        title: 'Registration successful',
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const Image(
+                height: 150,
+                width: 150,
+                image: AssetImage(
+                  'assets/images/Green_Check.png',
                 ),
-                SizedBox(
-                  height: 100,
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              const Text(
+                'You have successfully registered for Help for Hire!',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  'You have successfully registered for Help for Hire!!',
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              const Text(
+                'Sign in below in order to start using the application',
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                child: const Text(
+                  'SIGN IN',
                   style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
                 ),
-                SizedBox(
-                  height: 100,
-                ),
-                Text('Sign in below in order to start using the application'),
-                SizedBox(
-                  height: 50,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ],
-            ),
+                onPressed: () {},
+              ),
+            ],
           ),
         ),
       ),
