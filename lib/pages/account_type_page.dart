2794fbef8bb749baa7772523c4aa173d5b6cab41
Minch909 <wstander82@gmail.com>
2021-11-11 16:38:35 +0200

    Update import

--- lib/pages/account_type_page.dart
+++ lib/pages/account_type_page.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:help_for_hire_flutter_app/widgets/app_bar_widget.dart';
 
