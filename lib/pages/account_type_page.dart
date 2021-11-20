52ffc799de8512d815c095726aa5496f6bce6850
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-11-20 15:19:17 +0200

    Update account type page
    - This page still needs some improvements

--- lib/pages/account_type_page.dart
+++ lib/pages/account_type_page.dart
@@ -1,110 +1,73 @@
-// ignore_for_file: prefer_const_constructors, avoid_init_to_null
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:help_for_hire_flutter_app/routes/route_manager.dart';
-import 'package:help_for_hire_flutter_app/widgets/app_bars/app_bar_widget.dart';
 import 'package:help_for_hire_flutter_app/constants/image_constants.dart';
+import 'package:help_for_hire_flutter_app/routes/route_manager.dart';
+import 'package:help_for_hire_flutter_app/widgets/cards/account_type_card_widget.dart';
+import 'package:help_for_hire_flutter_app/widgets/dividers/divider_widget.dart';
+import 'package:help_for_hire_flutter_app/widgets/headers/header_widget.dart';
+import 'package:help_for_hire_flutter_app/widgets/spacers/large_spacer_widget.dart';
+import 'package:help_for_hire_flutter_app/widgets/spacers/medium_spacer_widget.dart';
+import 'package:help_for_hire_flutter_app/widgets/spacers/small_spacer_widget.dart';
 
-class AccountTypePage extends StatefulWidget {
-  const AccountTypePage({Key? key}) : super(key: key);
+class AccountTypePage extends StatelessWidget {
+  const AccountTypePage();
 
-  @override
-  _AccountTypePageState createState() => _AccountTypePageState();
-}
-
-class _AccountTypePageState extends State<AccountTypePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(
-        data: 'Account type',
-      ),
-      body: Center(
-        child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text(
-                    'please choose your account type:',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                )),
-            Expanded(
-              flex: 6,
-              child: AccountTypeCard(
-                text: 'Worker',
-                color: Colors.white,
-                isEmployer: false,
-                image: ImageConstants.worker,
-              ),
-            ),
-            Expanded(
-              flex: 6,
-              child: AccountTypeCard(
-                text: 'Employer',
-                color: Colors.white,
-                isEmployer: true,
-                image: ImageConstants.employer,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class AccountTypeCard extends StatelessWidget {
-  final text;
-  final color;
-  final isEmployer;
-  final image;
-  const AccountTypeCard({
-    Key? key,
-    required this.text,
-    required this.color,
-    required this.isEmployer,
-    required this.image,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
-      child: Container(
-        //can add pictures of the relevant account
-        decoration: BoxDecoration(
-            color: color, borderRadius: BorderRadius.all(Radius.circular(10))),
-        child: InkWell(
-          onTap: () {
-            if (isEmployer) {
-              Navigator.of(context).pushNamed(RouteManager.workerRegisterPage);
-            } else {
-              Navigator.of(context).pushNamed(RouteManager.workerRegisterPage);
-            }
-          },
+      backgroundColor: Colors.blue[900],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(
+            16.0,
+          ),
           child: Center(
-              child: Row(
-                mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Image.asset(image),
-              ),
-              Text(
-                text,
-                style: TextStyle(
-                  fontSize: 30,
-                  //color: Colors.white,
+            child: Column(
+              children: [
+                const HeaderWidget(
+                  data: 'Account Type',
                 ),
-              ),
-            ],
-          )),
+                const SmallSpacerWidget(),
+                const DividerWidget(
+                  height: 4.0,
+                  width: 256.0,
+                ),
+                const SmallSpacerWidget(),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Select\nan account type',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                    ),
+                  ),
+                ),
+                const LargeSpacerWidget(),
+                // Need an image here
+                AccountTypeCardWidget(
+                  data: 'Employer',
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    RouteManager.termsAndConditionsPage,
+                  ),
+                ),
+                const SmallSpacerWidget(),
+                const DividerWidget(
+                  height: 4.0,
+                  width: 192.0,
+                ),
+                const SmallSpacerWidget(),
+                // Need an image here
+                AccountTypeCardWidget(
+                  data: 'Worker',
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    RouteManager.workerRegisterPage,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
