1eb26b591ef576c2360ec793fb9900835745f201
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-11-23 13:47:16 +0200

    Fix the dropdown error

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:help_for_hire_flutter_app/constants/location_constants.dart';
 import 'package:help_for_hire_flutter_app/models/location_model.dart';
 import 'package:help_for_hire_flutter_app/services/location_service.dart';
+import 'package:help_for_hire_flutter_app/widgets/buttons/rounded_button_widget.dart';
 import 'package:help_for_hire_flutter_app/widgets/dividers/divider_widget.dart';
 import 'package:help_for_hire_flutter_app/widgets/gradients/blue_gradient_widget.dart';
 import 'package:help_for_hire_flutter_app/widgets/headers/header_widget.dart';
@@ -97,49 +98,53 @@ class _LocationPageState extends State<LocationPage> {
                               ),
                             ),
                             const MediumSpacerWidget(),
-                            Consumer<LocationService>(
-                              builder: (_, service, __) {
-                                return Container(
-                                  child: DropdownButtonFormField(
-                                    decoration: const InputDecoration(
-                                      enabledBorder: UnderlineInputBorder(
-                                        borderSide: BorderSide(
-                                          color: Colors.white,
+                            Container(
+                              child: DropdownButtonFormField(
+                                decoration: const InputDecoration(
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  labelText: 'City',
+                                ),
+                                iconSize: 28.0,
+                                items: context
+                                    .read<LocationService>()
+                                    .locations
+                                    .map(
+                                      (value) => DropdownMenuItem(
+                                        // Need this otherwise it breaks
+                                        value: value.city,
+                                        child: Text(
+                                          value.city,
                                         ),
                                       ),
-                                      labelText: 'City',
-                                    ),
-                                    iconSize: 28.0,
-                                    items: service.locations
-                                        .map(
-                                          (value) => DropdownMenuItem(
-                                            value: value,
-                                            child: Text(
-                                              value.city,
-                                            ),
-                                          ),
-                                        )
-                                        .toList(),
-                                    onChanged: (value) {},
+                                    )
+                                    .toList(),
+                                onChanged: (value) {},
+                              ),
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    8.0,
                                   ),
-                                  decoration: const BoxDecoration(
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(
-                                        8.0,
-                                      ),
-                                    ),
-                                    color: Colors.white,
-                                  ),
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 16.0,
-                                    vertical: 8.0,
-                                  ),
-                                );
-                              },
+                                ),
+                                color: Colors.white,
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0,
+                                vertical: 8.0,
+                              ),
                             ),
                           ],
                         ),
                       ),
+                    ),
+                    const MediumSpacerWidget(),
+                    RoundedButtonWidget(
+                      data: 'SUBMIT',
+                      onPressed: () {},
                     ),
                   ],
                 ),
