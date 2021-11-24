6cbe92f5aff0272ce7bbd6ffe5edb2da38a484a8
Minch909 <wstander82@gmail.com>
2021-11-24 21:36:14 +0200

    Update selected worker profile
    -  Unable to add service due to weird errors that I need to sort out on my pc

--- lib/pages/selected_worker_profile_page.dart
+++ lib/pages/selected_worker_profile_page.dart
@@ -1,9 +1,15 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:help_for_hire_flutter_app/models/worker_model.dart';
+import 'package:help_for_hire_flutter_app/services/user_service.dart';
+import 'package:help_for_hire_flutter_app/services/worker_service.dart';
 import 'package:help_for_hire_flutter_app/widgets/app_bars/app_bar_widget.dart';
+import 'package:help_for_hire_flutter_app/widgets/buttons/button_widget.dart';
 import 'package:help_for_hire_flutter_app/widgets/buttons/flat_button_widget.dart';
+import 'package:help_for_hire_flutter_app/widgets/buttons/text_button_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/src/provider.dart';
 
 class SelectedWorkerProfilePage extends StatelessWidget {
   // const SelectedWorkerProfilePage({
@@ -15,6 +21,8 @@ class SelectedWorkerProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    context.read<WorkerService>().getWorker;
+
     return Scaffold(
       appBar: AppBarWidget(
         data: 'Profile',
@@ -35,29 +43,14 @@ class SelectedWorkerProfilePage extends StatelessWidget {
                 height: 10,
               ),
               Text(
-                '${worker?.name} ${worker?.surname}',
+                'John Doe',
+                //'${worker?.name} ${worker?.surname}',
                 style: TextStyle(
                   fontSize: 20,
                 ),
               ),
-              Padding(
-                padding:
-                    EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    FlatButtonWidget(
-                      onPressed: () {},
-                      primary: Colors.blue,
-                      data: 'Rate user',
-                    ),
-                    FlatButtonWidget(
-                      onPressed: () {},
-                      primary: Colors.blue,
-                      data: 'Report user',
-                    ),
-                  ],
-                ),
+              SizedBox(
+                height: 20,
               ),
               Padding(
                 padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
@@ -75,7 +68,8 @@ class SelectedWorkerProfilePage extends StatelessWidget {
                       Padding(
                         padding: EdgeInsets.all(10),
                         child: Text(
-                          '${worker?.description}',
+                          'Description',
+                          //'${worker?.description}',
                           style: TextStyle(
                             fontSize: 15,
                           ),
@@ -85,36 +79,66 @@ class SelectedWorkerProfilePage extends StatelessWidget {
                   ),
                 ),
               ),
-              Row(
+              Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Text(
-                    'Minimum fee: ${worker?.minimumFee}',
+                    'Minimum Fee: R150',
+                    //'Minimum fee: ${worker?.minimumFee}',
                     style: TextStyle(
                       fontSize: 20,
                     ),
                   ),
-
+                  Text(
+                    'Full Time',
+                    //'Full Time: ${worker?.fullTime}',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    'Part Time',
+                    //'Part Time: ${worker?.partTime}',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
                   // worker.fullTime ? Text('Full Time') : Container(),
                   // worker.partTime ? Text('Part Time') : Container(),
+
+                  // add list of skills
+                  // list view builder
+                  Text('List skills here')
                 ],
               ),
               SizedBox(
                 height: 20,
               ),
-              Text(
-                'and so on...',
-                style: TextStyle(
-                  fontSize: 20,
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 5, right: 5, top: 20, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // put at bottom
+
+                    FlatButtonWidget(
+                      onPressed: () {},
+                      primary: Colors.blue,
+                      data: 'Rate user',
+                    ),
+                    FlatButtonWidget(
+                      onPressed: () {},
+                      primary: Colors.blue,
+                      data: 'View this users History',
+                    ),
+                    FlatButtonWidget(
+                      onPressed: () {},
+                      primary: Colors.blue,
+                      data: 'Report user',
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              FlatButtonWidget(
-                onPressed: () {},
-                primary: Colors.blue,
-                data: 'View this users History',
               ),
             ],
           ),
