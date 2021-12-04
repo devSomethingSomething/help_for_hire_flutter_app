77dc16e7a553a9ad4d6845d0b934082ab907be7d
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-12-04 14:15:36 +0200

    Add empty constructor to user model

--- lib/pages/invites_page.dart
+++ lib/pages/invites_page.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:help_for_hire_flutter_app/constants/color_constants.dart';
 import 'package:help_for_hire_flutter_app/enums/status.dart';
 import 'package:help_for_hire_flutter_app/extensions/string_extension.dart';
-import 'package:help_for_hire_flutter_app/helpers/color_helper.dart';
+import 'package:help_for_hire_flutter_app/helpers/delay_helper.dart';
 import 'package:help_for_hire_flutter_app/models/employer_model.dart';
 import 'package:help_for_hire_flutter_app/models/user_model.dart';
+import 'package:help_for_hire_flutter_app/routes/route_manager.dart';
 import 'package:help_for_hire_flutter_app/services/employer_service.dart';
 import 'package:help_for_hire_flutter_app/services/invite_service.dart';
 import 'package:help_for_hire_flutter_app/services/user_service.dart';
@@ -19,14 +20,14 @@ class InvitesPage extends StatelessWidget {
   const InvitesPage();
 
   /// Get the invites for the current user based on their account type
-  void _getInvitesForUser({
+  Future<void> _getInvitesForUser({
     required BuildContext context,
-  }) =>
+  }) async =>
       context.read<UserService>().isEmployer
-          ? context.read<InviteService>().getInvitesForEmployer(
+          ? await context.read<InviteService>().getInvitesForEmployer(
                 employerId: context.read<UserService>().currentUser.userId,
               )
-          : context.read<InviteService>().getInvitesForWorker(
+          : await context.read<InviteService>().getInvitesForWorker(
                 id: context.read<UserService>().currentUser.userId,
               );
 
@@ -45,6 +46,7 @@ class InvitesPage extends StatelessWidget {
         locationId: '123',
       ),
     );
+    // ---------------------------
 
     // Gets the invites for the current user
     _getInvitesForUser(
@@ -58,9 +60,15 @@ class InvitesPage extends StatelessWidget {
             icon: const Icon(
               Icons.refresh,
             ),
-            onPressed: () => _getInvitesForUser(
-              context: context,
-            ),
+            onPressed: () async {
+              DelayHelper.showLoadingIndicator(context: context);
+
+              await _getInvitesForUser(
+                context: context,
+              );
+
+              DelayHelper.hideLoadingIndicator(context: context);
+            },
           ),
         ],
         data: 'Invites',
@@ -74,132 +82,134 @@ class InvitesPage extends StatelessWidget {
               )
             : Center(
                 child: ListView.builder(
-                  // Card should be clickable, should be able to go to the
-                  // profile of the user
-                  itemBuilder: (_, index) => Card(
-                    child: Padding(
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.person,
-                            // Perhaps the color should change depending on the
-                            // invite status
-                            color: ColorHelper.getRandomColor(),
-                            size: 48.0,
-                          ),
-                          const SizedBox(
-                            width: 8.0,
-                          ),
-                          // Will have to change
-                          context.read<UserService>().isEmployer
-                              ? ChangeNotifierProvider<WorkerService>(
-                                  create: (_) => WorkerService(),
-                                  builder: (_, __) => Consumer<WorkerService>(
-                                    builder: (_, workerService, __) {
-                                      workerService.getWorker(
-                                        id: service.invites[index].workerId,
-                                      );
-
-                                      context.read<UserService>().selectedUser =
-                                          workerService.worker ??
-                                              UserModel(
-                                                userId: '',
-                                                name: '',
-                                                surname: '',
-                                                phoneNumber: '',
-                                                locationId: '',
-                                              );
-
-                                      return Text(
-                                        '${workerService.worker?.name ?? 'John'} '
-                                        '${workerService.worker?.surname ?? 'Doe'}',
-                                      );
-                                    },
-                                  ),
-                                )
-                              : ChangeNotifierProvider<EmployerService>(
-                                  create: (_) => EmployerService(),
-                                  builder: (_, __) => Consumer<EmployerService>(
-                                    builder: (_, employerService, __) {
-                                      employerService.getEmployer(
-                                        id: service.invites[index].employerId,
-                                      );
-
-                                      context.read<UserService>().selectedUser =
-                                          employerService.employer ??
-                                              UserModel(
-                                                userId: '',
-                                                name: '',
-                                                surname: '',
-                                                phoneNumber: '',
-                                                locationId: '',
-                                              );
-
-                                      return Text(
-                                        '${employerService.employer?.name ?? 'John'} '
-                                        '${employerService.employer?.surname ?? 'Doe'}',
-                                      );
-                                    },
-                                  ),
-                                ),
-                          const Spacer(),
-                          context.read<UserService>().isEmployer
-                              ? service.invites[index].status ==
-                                      Status.accepted
+                  itemBuilder: (_, index) => GestureDetector(
+                    child: Card(
+                      child: Padding(
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.person,
+                              color: service.invites[index].status ==
+                                      Status.pending
                                           .toString()
                                           .replaceAll('Status.', '')
                                           .capitalize()
-                                  ? FlatButtonWidget(
-                                      data: 'CONTACT',
-                                      onPressed: () => launch(
-                                        'tel://${context.read<UserService>().selectedUser.phoneNumber}',
-                                      ),
-                                      primary: Colors.green,
-                                    )
-                                  : Text(
-                                      service.invites[index].status,
-                                    )
-                              : service.invites[index].status ==
-                                      Status.accepted
-                                          .toString()
-                                          .replaceAll('Status.', '')
-                                          .capitalize()
-                                  ? Text(
-                                      service.invites[index].status,
-                                    )
-                                  : FlatButtonWidget(
-                                      data: 'ACCEPT',
-                                      onPressed: () {
-                                        service.invites[index].status = Status
-                                            .accepted
-                                            .toString()
-                                            .replaceAll('Status.', '')
-                                            .capitalize();
+                                  ? Colors.blue[200]
+                                  : Colors.blue,
+                              size: 40.0,
+                            ),
+                            const SizedBox(
+                              width: 8.0,
+                            ),
+                            context.read<UserService>().isEmployer
+                                ? ChangeNotifierProvider<WorkerService>(
+                                    create: (_) => WorkerService(),
+                                    builder: (_, __) => Consumer<WorkerService>(
+                                      builder: (_, workerService, __) {
+                                        workerService.getWorker(
+                                          id: service.invites[index].workerId,
+                                        );
 
-                                        service.putInvite(
-                                          id: service.invites[index].inviteId,
-                                          invite: service.invites[index],
+                                        context
+                                                .read<UserService>()
+                                                .selectedUser =
+                                            workerService.worker ??
+                                                UserModel.empty();
+
+                                        return Text(
+                                          '${workerService.worker?.name ?? 'John'} '
+                                          '${workerService.worker?.surname ?? 'Doe'}',
                                         );
                                       },
-                                      primary: Colors.green,
                                     ),
-                          const SizedBox(
-                            width: 8.0,
-                          ),
-                          FlatButtonWidget(
-                            data: 'DELETE',
-                            onPressed: () => service.deleteInvite(
-                              id: service.invites[index].inviteId,
+                                  )
+                                : ChangeNotifierProvider<EmployerService>(
+                                    create: (_) => EmployerService(),
+                                    builder: (_, __) =>
+                                        Consumer<EmployerService>(
+                                      builder: (_, employerService, __) {
+                                        employerService.getEmployer(
+                                          id: service.invites[index].employerId,
+                                        );
+
+                                        context
+                                                .read<UserService>()
+                                                .selectedUser =
+                                            employerService.employer ??
+                                                UserModel.empty();
+
+                                        return Text(
+                                          '${employerService.employer?.name ?? 'John'} '
+                                          '${employerService.employer?.surname ?? 'Doe'}',
+                                        );
+                                      },
+                                    ),
+                                  ),
+                            const Spacer(),
+                            context.read<UserService>().isEmployer
+                                ? service.invites[index].status ==
+                                        Status.accepted
+                                            .toString()
+                                            .replaceAll('Status.', '')
+                                            .capitalize()
+                                    ? FlatButtonWidget(
+                                        data: 'CONTACT',
+                                        onPressed: () => launch(
+                                          'tel://${context.read<UserService>().selectedUser.phoneNumber}',
+                                        ),
+                                        primary: Colors.green,
+                                      )
+                                    : Text(
+                                        service.invites[index].status,
+                                      )
+                                : service.invites[index].status ==
+                                        Status.accepted
+                                            .toString()
+                                            .replaceAll('Status.', '')
+                                            .capitalize()
+                                    ? Text(
+                                        service.invites[index].status,
+                                      )
+                                    : FlatButtonWidget(
+                                        data: 'ACCEPT',
+                                        onPressed: () {
+                                          service.invites[index].status = Status
+                                              .accepted
+                                              .toString()
+                                              .replaceAll('Status.', '')
+                                              .capitalize();
+
+                                          service.putInvite(
+                                            id: service.invites[index].inviteId,
+                                            invite: service.invites[index],
+                                          );
+                                        },
+                                        primary: Colors.green,
+                                      ),
+                            const SizedBox(
+                              width: 8.0,
                             ),
-                            primary: Colors.red,
-                          ),
-                        ],
+                            FlatButtonWidget(
+                              data: 'DELETE',
+                              onPressed: () => service.deleteInvite(
+                                id: service.invites[index].inviteId,
+                              ),
+                              primary: Colors.red,
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.all(
+                          8.0,
+                        ),
                       ),
-                      padding: const EdgeInsets.all(
-                        8.0,
-                      ),
+                      elevation: 4.0,
                     ),
-                    elevation: 4.0,
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      context.read<UserService>().isEmployer
+                          ? RouteManager.selectedEmployerProfilePage
+                          : RouteManager.selectedWorkerProfilePage,
+                    ),
                   ),
                   itemCount: service.invites.length,
                 ),
