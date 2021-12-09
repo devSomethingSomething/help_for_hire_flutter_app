9ace725436ca48c6315aecde3a1ae46bdbf97a51
night-crow <afhan2000@gmail.com>
2021-12-09 13:01:16 +0200

    update three additional pages
    added consumer to all pages.
    
    replaced context.read to user valuses from the consumer.

--- lib/pages/selected_employer_profile_page.dart
+++ lib/pages/selected_employer_profile_page.dart
@@ -19,164 +19,167 @@ class SelectedEmployerProfilePage extends StatelessWidget {
     context.read<LocationService>().getLocation(
           id: context.read<EmployerService>().employer?.locationId ?? '',
         );
-
-    return Scaffold(
-      appBar: AppBarWidget(
-        // Will need to add checks to ensure that the selected profile gets the
-        // info from either the employer service or the user service as we have
-        // the data in two places
-        // This could be done by checking if a single value is empty for a
-        // service and then just assigning the values or not
-        data: '${context.read<EmployerService>().employer?.name} '
-            '${context.read<EmployerService>().employer?.surname}',
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  Padding(
-                    child: Card(
-                      child: Padding(
-                        child: SizedBox(
-                          child: Column(
-                            children: [
-                              const SizedBox(),
-                              const Spacer(
-                                flex: 3,
-                              ),
-                              Text(
-                                '${context.read<EmployerService>().employer?.name} '
-                                '${context.read<EmployerService>().employer?.surname}',
-                                style: const TextStyle(
-                                  fontSize: 28.0,
-                                  fontWeight: FontWeight.bold,
+    return Consumer<EmployerService>(
+    builder: (context, value, child) {
+      return Scaffold(
+        appBar: AppBarWidget(
+          // Will need to add checks to ensure that the selected profile gets the
+          // info from either the employer service or the user service as we have
+          // the data in two places
+          // This could be done by checking if a single value is empty for a
+          // service and then just assigning the values or not
+          data: '${value.employer?.name} '
+              '${value.employer?.surname}',
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Padding(
+                      child: Card(
+                        child: Padding(
+                          child: SizedBox(
+                            child: Column(
+                              children: [
+                                const SizedBox(),
+                                const Spacer(
+                                  flex: 3,
                                 ),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SmallSpacerWidget(),
-                              const Text(
-                                'Shaw Inc.',
-                                style: TextStyle(
-                                  fontSize: 24.0,
+                                Text(
+                                  '${value.employer?.name} '
+                                      '${value.employer?.surname}',
+                                  style: const TextStyle(
+                                    fontSize: 28.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.center,
                                 ),
-                                textAlign: TextAlign.center,
-                              ),
-                              const Spacer(),
-                            ],
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                                const SmallSpacerWidget(),
+                                const Text(
+                                  'Shaw Inc.',
+                                  style: TextStyle(
+                                    fontSize: 24.0,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const Spacer(),
+                              ],
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                            ),
+                            height: 175.0,
                           ),
-                          height: 175.0,
+                          padding: const EdgeInsets.all(
+                            16.0,
+                          ),
                         ),
-                        padding: const EdgeInsets.all(
-                          16.0,
-                        ),
+                        elevation: 4.0,
                       ),
-                      elevation: 4.0,
+                      padding: const EdgeInsets.only(
+                        top: 75.0,
+                      ),
                     ),
-                    padding: const EdgeInsets.only(
-                      top: 75.0,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: CircleAvatar(
-                      backgroundColor: ColorConstants.darkBlue,
+                    Align(
+                      alignment: Alignment.topCenter,
                       child: CircleAvatar(
-                        backgroundColor: ColorConstants.white,
-                        child: Icon(
-                          Icons.person,
-                          color: ColorConstants.darkBlue,
-                          size: 128.0,
-                        ),
-                        radius: 70.0,
-                      ),
-                      radius: 75.0,
-                    ),
-                  ),
-                ],
-              ),
-              Card(
-                child: Padding(
-                  child: SizedBox(
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Other Details',
-                          style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
+                        backgroundColor: ColorConstants.darkBlue,
+                        child: CircleAvatar(
+                          backgroundColor: ColorConstants.white,
+                          child: Icon(
+                            Icons.person,
+                            color: ColorConstants.darkBlue,
+                            size: 128.0,
                           ),
-                          textAlign: TextAlign.center,
+                          radius: 70.0,
                         ),
-                        const Divider(
-                          thickness: 2.0,
-                        ),
-                        const SmallSpacerWidget(),
-                        Row(
-                          children: [
-                            const Text(
-                              'Province:',
-                              style: TextStyle(
-                                fontSize: 20.0,
-                              ),
-                            ),
-                            Consumer<LocationService>(
-                              builder: (_, service, __) {
-                                return Text(
-                                  service.location?.province ?? 'Province',
-                                  style: const TextStyle(
-                                    fontSize: 20.0,
-                                  ),
-                                );
-                              },
-                            ),
-                          ],
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        ),
-                        const Divider(),
-                        const SmallSpacerWidget(),
-                        Row(
-                          children: [
-                            const Text(
-                              'City:',
-                              style: TextStyle(
-                                fontSize: 20.0,
-                              ),
-                            ),
-                            Consumer<LocationService>(
-                              builder: (_, service, __) {
-                                return Text(
-                                  service.location?.city ?? 'City',
-                                  style: const TextStyle(
-                                    fontSize: 20.0,
-                                  ),
-                                );
-                              },
-                            ),
-                          ],
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        ),
-                        const Divider(),
-                      ],
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                        radius: 75.0,
+                      ),
                     ),
-                    height: 175.0,
-                  ),
-                  padding: const EdgeInsets.all(
-                    16.0,
-                  ),
+                  ],
                 ),
-                elevation: 4.0,
-              ),
-            ],
+                Card(
+                  child: Padding(
+                    child: SizedBox(
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Other Details',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const Divider(
+                            thickness: 2.0,
+                          ),
+                          const SmallSpacerWidget(),
+                          Row(
+                            children: [
+                              const Text(
+                                'Province:',
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                              Consumer<LocationService>(
+                                builder: (_, service, __) {
+                                  return Text(
+                                    service.location?.province ?? 'Province',
+                                    style: const TextStyle(
+                                      fontSize: 20.0,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          ),
+                          const Divider(),
+                          const SmallSpacerWidget(),
+                          Row(
+                            children: [
+                              const Text(
+                                'City:',
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                              Consumer<LocationService>(
+                                builder: (_, service, __) {
+                                  return Text(
+                                    service.location?.city ?? 'City',
+                                    style: const TextStyle(
+                                      fontSize: 20.0,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          ),
+                          const Divider(),
+                        ],
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                      ),
+                      height: 175.0,
+                    ),
+                    padding: const EdgeInsets.all(
+                      16.0,
+                    ),
+                  ),
+                  elevation: 4.0,
+                ),
+              ],
+            ),
+          ),
+          padding: const EdgeInsets.all(
+            16.0,
           ),
         ),
-        padding: const EdgeInsets.all(
-          16.0,
-        ),
-      ),
-    );
+      );
+    });
+
   }
 }
